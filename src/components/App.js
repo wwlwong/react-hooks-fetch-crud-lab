1563f70d4a4ed0bc1ae16cdf947e8643a111b2de
William Wong <william.wai.lim.wong@gmail.com>
2023-06-06 14:16:49 -0400

    passes tests

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm questions={questions} setQuestions={setQuestions}/> : <QuestionList questions={questions} setQuestions={setQuestions} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList( {questions, setQuestions} ) {
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => {
+        console.log(questions);
+        setQuestions(questions)});
+  }, []); 
+
+  function onDeleteQuestion(id){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+    .then((r) => r.json())
+    .then(() => setQuestions(questions.filter((question) => question.id !== id)));
+  }
+  
+  function onAnswerChange(id, updatedIndex){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify( {
+        "correctIndex" : updatedIndex,
+      })
+    })
+    .then((r) => r.json())
+    .then((updatedQuestion) => {
+      const updatedQuestions = questions.map((question) => {
+        if (question.id === id) return updatedQuestion;
+        return question;
+      });
+      setQuestions(updatedQuestions);
+    });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul> {questions.map((question) => (
+        <QuestionItem key={question.id} question={question} onDeleteQuestion={onDeleteQuestion} onAnswerChange={onAnswerChange} /> ))} 
+        </ul>
     </section>
   );
 }
